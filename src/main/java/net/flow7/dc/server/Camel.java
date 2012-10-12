e7bdc1c838bc643cf4d9796b2f608a1b83e63f09
David Dale <davidadale@gmail.com>
2012-10-12 14:11:20 -0400

    adds pdfbox and a route to print labels

--- src/main/java/net/flow7/dc/server/Camel.java
+++ src/main/java/net/flow7/dc/server/Camel.java
@@ -15,11 +15,14 @@
  */
 package net.flow7.dc.server;
 
+import java.io.File;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.aws.s3.S3Constants;
+import org.apache.camel.component.file.GenericFile;
 import org.apache.camel.dataformat.tika.TikaDataFormat;
 import org.apache.camel.spi.DataFormat;
+import org.apache.pdfbox.PrintPDF;
 
 /**
  *
@@ -35,7 +38,19 @@ public class Camel extends RouteBuilder{
 
     public void configure() throws Exception {
         from("file://test")
-        .unmarshal( tika  )
+        .process(new Processor() {
+            public void process(Exchange ex) throws Exception {
+                GenericFile load = (GenericFile) ex.getIn().getBody();
+                File file = (File) load.getFile();
+                System.out.println("((((((" + file.getPath() + "))))))))");
+                String[] args = new String[]{"-silentPrint", file.getAbsolutePath() };
+                PrintPDF.main( args );
+            }
+        });
+                
+                
+                //.to("lpr://localhost/default");
+                /*.unmarshal( tika  )
         .choice()
         //.when(header("TikaContentType").isEqualTo( "text/plain; charset=ISO-8859-1" )).to( "http://localhost:9000/api/photo" )        
         //.when(header("TikaContentType").isEqualTo("audio/mp4")).to( "http://localhost:9000/api/song" )      
@@ -44,7 +59,7 @@ public class Camel extends RouteBuilder{
         .setHeader(Exchange.HTTP_QUERY, simple("item.filename=${file:name}&item.filetype=audio&item.identifier=asdf&item.orderNumber=${file:parent}&item.size=${file:size}") )                        
         .to("http://localhost:9000/api/order/item")
         .setHeader(S3Constants.KEY, header("CamelFileNameOnly") )                        
-        .to("aws-s3://flow7?accessKey=AKIAIFJQDYJQ462ABPQQ&secretKey=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&region=us-east-1");
+        .to("aws-s3://flow7?accessKey=AKIAIFJQDYJQ462ABPQQ&secretKey=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&region=us-east-1");*/
         
     }
 }
